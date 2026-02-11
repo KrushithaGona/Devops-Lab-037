3c382d8f0b2baf94967475a552dc7ca0f0a8d9d6
KrushithaGona <245123751037@mvsrec.edu.in>
2026-02-11 13:09:17 +0530

    add.c file

---
+++ krushi/add.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    // Taking input
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    // Adding
+    sum = num1 + num2;
+
+    // Display result
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}
+
